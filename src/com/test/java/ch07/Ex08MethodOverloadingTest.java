9fa06f93a193ac9b82f5d4e48ef5a5956cfa7bae
rlawlstjd <kjsng20@gmail.com>
2024-08-26 07:44:05 +0900

    0000

--- src/com/test/java/ch07/Ex08MethodOverloadingTest.java
+++ src/com/test/java/ch07/Ex08MethodOverloadingTest.java
@@ -1,5 +1,20 @@
 package com.test.java.ch07;
 
 public class Ex08MethodOverloadingTest {
-
+	public static void main(String[] args) {
+	
+	
+	
+	
+	s
+	
+	}
 }
+/*
+문제 1.
+자동차 정보 자동차이름(문자열), 최대속도(정수), 자동차회사(문자열) 를 저장하는 클래스를 만들고
+각각의 속성을 설정하는 메소드를 작성 후 출력하세요.
+
+문제 2.
+속성을 설정하는 메소드를 오버로딩을 사용하여 작성 후 출력하세요. 
+*/
