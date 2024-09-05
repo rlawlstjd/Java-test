1093b2f703575054a6f154644347e0a57277f3e4
rlawlstjd <kjsng20@gmail.com>
2024-09-05 11:50:10 +0900

    ㅇㅇㅇ

--- src/com/test/java/ch11/TEst.java
+++ src/com/test/java/ch11/TEst.java
@@ -6,10 +6,20 @@ public class TEst {
 				
 		System.out.println("오늘 날씨는 " + todayWeather);
 		
-				
-		Day toDay = Day.FRIDAY; 
 		
-		System.out.println("오늘의 요일은 " + toDay); 
+		switch (todayWeather) {
+			case SUNNY: 
+				System.out.println("오늘 날씨는 맑아요!");
+				break; 
+			case CLOUDY: 
+				System.out.println("오늘은 흐려요. 우산을 챙기면 좋겠습니다.");
+				break;
+			case RAINY: 
+				System.out.println("오늘은 비가 와요. 우산을 꼭 챙기세요.");
+				break;
+		
+		}
+		
 		
 	}
 }
