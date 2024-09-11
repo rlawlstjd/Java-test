0a991fc025a91972fe27024a71cbffe6eb5f0f42
rlawlstjd <kjsng20@gmail.com>
2024-09-11 09:06:28 +0900

    Update Ex08ChainedExceptionTest.java

--- src/com/test/java/ch12/Ex08ChainedExceptionTest.java
+++ src/com/test/java/ch12/Ex08ChainedExceptionTest.java
@@ -4,65 +4,61 @@ import java.util.Scanner;
 
 public class Ex08ChainedExceptionTest {
 	public static void main(String[] args) {
-		VendingMachine1 vd = new VendingMachine1(); 
+		while(true) {
 		
-		BerverageChoice bc = VendingMachine1.form();
-		
-		System.out.println(bc.getChoice());
-		System.out.println(bc.getMoney());
-		
-	
+			Scanner scan = new Scanner (System.in); 
+			
+			System.out.println("---자판기---");
+			System.out.println("음료를 선택하세요.");
+			System.out.println("1:사과맛 2:포도맛 3:망고맛"); 
+			int choice = scan.nextInt();
+			
+			if (choice < 1 || choice > 3) {
+				System.out.printf("잘못된 선택입니다. 시스템을 종료합니다.");
+				break;
+			}
+			
+			System.out.println("금액을 입력하세요.");
+			int money = scan.nextInt(); 
+			
+			try {
+				if(VendingMachine1.apple == money) {
+					throw new 
+				}
+			}
+		}
 	}
 }
+
+class VendingMachine
+
+
 
 class VendingMachine1 {
-	private static final int apple = 1000; 
-	private static final int grape = 1200;
-	private static final int mango = 1500; 
+	static final int apple = 1000; 
+	static final int grape = 1200;
+	static final int mango = 1500; 
+}
 	
-	
-	public static BerverageChoice form() {
-		BerverageChoice bc = new BerverageChoice();
-		Scanner scan = new Scanner (System.in); 
-		
-		System.out.println("---자판기---");
-		System.out.println("음료를 선택하세요.");
-		System.out.println("1:사과맛 2:포도맛 3:망고맛"); 
-		int choice = scan.nextInt();
-		
-		System.out.println("금액을 입력하세요");
-		int money = scan.nextInt();
-		
-		bc.setChoice(choice);
-		bc.setMoney(money);
-		
-		return bc;
-		
-	}
 
+class VendingmachineException extends Exception {
+	public VendingmachineException (String message, Exception e) {
+		super (message, e);
+	}
 }
 
+class InsufficientException extends Exception {
+	
+	
+	public InsufficientException (String message) {
+		super (message);
+	}
+}
 
-class BerverageChoice {
-	private int choice; 
-	private int money; 
-	
-	public void setChoice(int choice) {
-		this.choice= choice;
+class EmptyException extends Exception {
+	public EmptyException (String message) {
+		super(message);
 	}
-	
-	public int getChoice() {
-		return choice;
-	}
-	
-	public void setMoney(int money) {
-		this.money = money;
-	}
-	
-	public int getMoney () {
-		return money;
-	}
-
 }
 /*문제 1.
 자판기 프로그램에 연결 예외를 적용해 주세요.
