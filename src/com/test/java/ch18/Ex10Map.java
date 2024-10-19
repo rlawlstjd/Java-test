d98d6fc3e5e7a48775e424da91cb04d519610d7b
rlawlstjd <kjsng20@gmail.com>
2024-10-19 22:31:28 +0900

    ..

--- src/com/test/java/ch18/Ex10Map.java
+++ src/com/test/java/ch18/Ex10Map.java
@@ -1,10 +1,29 @@
 package com.test.java.ch18;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ex10Map {
 	public static void main (String [] args) {
 		
+		System.out.println("< 팀A >"); 
+		Map<String, String> teamA = new HashMap<>(); 
+		teamA.put("A", "자바"); 
+		teamA.put("B", "C"); 
+		teamA.put("C", "파이썬");
+		teamA.put("D", "자바");
+		teamA.put("E", "자바");
+		teamA.put("E", "DB");
 		
+		for(Map.Entry<String, String> e : teamA.entrySet()) {
+			System.out.println("이름: " + e.getKey() + "\t스킬: " + e.getValue());
+		}				
 		
+		System.out.println(); 
+		
+		System.out.println("< 팀8 >"); 
+		Map<String, String> team8 = new HashMap<>(); 
+				
 		
 	}
 }
