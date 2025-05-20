549383158398dac773cda24fc7a4fa678ea9af29
Kim Jinseong <kjsng20@gmail.com>
2025-05-20 15:16:35 +0900

    .

--- src/RealTest/test.java
+++ src/RealTest/test.java
@@ -1,5 +1,19 @@
 package RealTest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class test {
-	asdㄴ
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        System.out.println("두 정수를 입력하세요"); 
+        String line = br.readLine(); 
+        String[] parts = line.split(" "); 
+        int a = Integer.parseInt(parts[0]); 
+        int b = Integer.parseInt(parts[1]);
+        
+        // 예: 결과 출력
+        System.out.println(a + b);
+    }
 }
